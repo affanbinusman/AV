d49d93db22444502878f7d436acde04d34abdfd0
Matt Watson <mjwats10@asu.edu>
2023-04-20 12:29:29 -0700

    added 360 scan of surroundings when no blocks detected

--- EV/robot_control.py
+++ EV/robot_control.py
@@ -3,20 +3,23 @@ import numpy as np
 import cv2 as cv
 import ev3_dc as ev3
 from thread_task import Sleep
+import time
 
 # -------- INIT CONSTS --------
 
 img_width = 1920
 img_height = 1440
-thresh = 0.3 # confidence threshold
+thresh = 0.5 # confidence threshold
 centerline = 0.5 # value between 0 and 1 indicating the horizontal postition of vehicle center in the image
-deadband = 0.025 # block must be at least this far from centerline to change steering
 sweep_y_min = 0.9 # min y-value to be in range for removal
-sweep_x_min = 0.4 # min x-value to be in range for removal
-sweep_x_max = 0.8 # max x-value to be in range for removal
+sweep_x_min = 0 # min x-value to be in range for removal
+sweep_x_max = 1 # max x-value to be in range for removal
 is_moving = False # indicates whether vehicle is moving
-speed = 5 # vehicle movement speed
-turn_radius = 100 # turn radius
+fwd_speed = 20 # vehicle movement fwd_speed
+max_turn_speed = 100 # max turn speed
+search_length = 9 # search time allotment before exiting
+search_speed = 10 # speed during search
+search_turn = -200 # spin in place during search
 
 # ----- SETUP VEHICLE --------
 
@@ -42,6 +45,60 @@ sweep = ( # design sweep action
 	sweep_motor.stop_as_task(brake=False)
 )
 
+# -------- DEFINE FUNCs ---------
+
+# run detection
+def get_dets(cap, model):
+	# Capture frame-by-frame
+	ret, img = cap.read()
+	
+	# Check if frame is read correctly, exit if not
+	if not ret:
+		print("Can't receive frame (stream end?). Exiting ...")
+		exit()
+	
+	# convert img to RGB
+	img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+	
+	# Run inference on the current frame
+	results = model(img)
+	
+	# Show bounding boxes and labels on the current frame
+	results.render()
+	
+	# return color to open-cv standard
+	img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+
+	# Display the resulting frame
+	cv.imshow('frame', img)
+	
+	# get detections then filter out low confidence instances
+	det_all = results.xyxy[0].detach().numpy()
+	keep_mask = det_all[:,4] > thresh
+	return det_all[keep_mask,:]
+
+# start search if no blocks detected
+def search():	
+	# start search
+	start_time = time.time()
+	vehicle.move(search_speed, search_turn)
+	is_moving = True
+	
+	# exit if block detected or search time expired
+	while True:
+		det = get_dets(cap, model)
+		if len(det) > 0:
+			# resume running
+			vehicle.stop()
+			is_moving = False
+			return
+		current_time = time.time()
+		if current_time - start_time > search_length:
+			# exit program
+			vehicle.stop()
+			is_moving = False
+			exit()
+
 # -------- RUN ----------
 
 # open camera, exit if not opening
@@ -52,60 +109,42 @@ if not cap.isOpened():
 	exit()
 
 # Load YOLOv5 model
-model = torch.hub.load('ultralytics/yolov5', 'custom', path=r'/Users/matt/Downloads/best (1).pt')
+model = torch.hub.load('ultralytics/yolov5', 'custom', path=r'/Users/matt/Downloads/best (2).pt')
 
+# main loop
 while True:
-	# Capture frame-by-frame
-	ret, img = cap.read()
-	
-	# Check if frame is read correctly, exit if not
-	if not ret:
-		print("Can't receive frame (stream end?). Exiting ...")
-		break
-	
-	# convert img to RGB
-	img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-	
-	# Run inference on the current frame
-	results = model(img)
-	
-	# Show bounding boxes and labels on the current frame
-	results.render()
-
-	# Display the resulting frame
-	cv.imshow('frame', img)
+	# get detections
+	dets = get_dets(cap, model)
 	
 	# exit on command
 	if cv.waitKey(1) == ord('q'):
+		if is_moving:
+			vehicle.stop()
+			is_moving = False
 		break
 	
-	# get detections
-	det_all = results.xyxy[0].detach().numpy()
-	
-	# throw out detections with confidence < threshold
-	keep_mask = det_all[:,4] > thresh
-	det_real = det_all[keep_mask,:]
-	
 	# get x-val and y-val of bbox center point of all confident detections
-	ul_y = det_real[:,1]
-	lr_y = det_real[:,3]
+	ul_y = dets[:,1]
+	lr_y = dets[:,3]
 	center_y = ((ul_y + lr_y) / 2) / img_height
 	
-	ul_x = det_real[:,0]
-	lr_x = det_real[:,2]
+	ul_x = dets[:,0]
+	lr_x = dets[:,2]
 	center_x = ((ul_x + lr_x) / 2) / img_width
 	
-	# determine if there is a "bad" block in position for removal
-	if len(det_real) > 0:
+	# see if there is a "bad" block to sweep
+	if len(dets) > 0:
+		# analyze block closest to vehicle
 		lowest_idx = np.argmax(center_y)
 		lowest_y = center_y[lowest_idx]
 		lowest_x = center_x[lowest_idx]
-		lowest_cls = det_real[lowest_idx,5]
+		lowest_cls = dets[lowest_idx,5]
+		# if there is a bad block in range to sweep, execute sweep action
 		if lowest_y >= sweep_y_min and lowest_x >= sweep_x_min and lowest_x <= sweep_x_max and lowest_cls != 1:
 			# remove brick entries from detection and center arrays
-			np.delete(center_y, lowest_idx, 0)
-			np.delete(center_x, lowest_idx, 0)
-			np.delete(det_real, lowest_idx, 0)
+			center_y = np.delete(center_y, lowest_idx, 0)
+			center_x = np.delete(center_x, lowest_idx, 0)
+			dets = np.delete(dets, lowest_idx, 0)
 			
 			# stop drive motors to sweep
 			if is_moving:
@@ -116,33 +155,24 @@ while True:
 			sweep.start()
 			sweep.join()
 			sweep.stop()
+		# no more blocks detected, run search
 	else:
-		# no more blocks, stop drive motors
-		if is_moving:
-			vehicle.stop()
-			is_moving = False
+		search()
 		continue
 	
 	# navigate to next brick with largest y-val (lowest in frame)
-	if len(det_real) > 0:
+	if len(dets) > 0:
 		# calculate proportional turn radius
 		lowest_idx = np.argmax(center_y)
 		lowest_x = center_x[lowest_idx]
-		if lowest_x < centerline - deadband: # block is left of center, turn left
-			turn = turn_radius * -1
-		elif lowest_x > centerline + deadband: # block is right of center, turn right
-			turn = turn_radius
-		else: # block is dead ahead, go straight
-			turn = 0
+		turn = int((lowest_x - 0.5) * 2 * max_turn_speed)
 		
 		# move
-		vehicle.move(speed, turn)
+		vehicle.move(fwd_speed, turn)
 		is_moving = True
 	else:
-		# no more blocks, stop drive motors
-		if is_moving:
-			vehicle.stop()
-			is_moving = False
+		# no more blocks, run search
+		search()
 		continue
 
 # Release the capture and destroy the window
